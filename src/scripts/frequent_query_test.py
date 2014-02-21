36fc0112305a5035a113ae2b926d33e777a00a75
hubert <hubertszostek@gmail.com>
2014-02-21 17:59:09 +0100

    histogram of frequent query

--- src/scripts/frequent_query_test.py
+++ src/scripts/frequent_query_test.py
@@ -4,15 +4,26 @@ try:
 except:
     pass
 import retrieval_system, environment, evaluation, query
+import os
 
-
-learner = retrieval_system.ListwiseLearningSystem(64, '-w random -c comparison.ProbabilisticInterleave -r ranker.ProbabilisticRankingFunction -s 3 -d 0.1 -a 0.01')
+#os.chdir("..")
+#os.chdir("..")
+#os.chdir("..")
+feature_count=136
+#feature_count=64
+learner = retrieval_system.ListwiseLearningSystem(feature_count, '-w random -c comparison.ProbabilisticInterleave -r ranker.ProbabilisticRankingFunction -s 3 -d 0.1 -a 0.01')
 user_model = environment.CascadeUserModel('--p_click 0:0.0,1:1 --p_stop 0:0.0,1:0.0')
 evaluation = evaluation.NdcgEval()
-training_queries = query.load_queries(sys.argv[1], 64)
+training_queries = query.load_queries(sys.argv[1], feature_count)
+query_freq={}
 for train in training_queries:
-    print len(train.__labels__)
-test_queries = query.load_queries(sys.argv[2], 64)
+    
+    if(len(train.__labels__) in query_freq):
+        query_freq[len(train.__labels__)]=query_freq[len(train.__labels__)]+1
+    else:
+        query_freq[len(train.__labels__)]=1    
+print query_freq                                 
+test_queries = query.load_queries(sys.argv[2], feature_count)
 while True:
     q = training_queries[random.choice(training_queries.keys())]
     l = learner.get_ranked_list(q)
