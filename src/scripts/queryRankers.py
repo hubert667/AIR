7e16bb3b072b9f4acbaa65e0061d7b2870fb07a4
hubert <hubertszostek@gmail.com>
2014-02-22 10:39:01 +0100

    error correction for pickle

--- src/scripts/queryRankers.py
+++ src/scripts/queryRankers.py
@@ -2,10 +2,13 @@
 
 class queryRankers:
     
-    query_ranker={}
+    #query_ranker={}
+    
+    def __init__(self):
+        self.query_ranker = {}  # an instance attribute
     
     def add(self,query,ranker):
         self.query_ranker[query]=ranker
         
-    #def save(self,path):
+  
         
