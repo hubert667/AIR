4ca89ca4313cc426d3cf8b414383e7ccb48196cb
hubert <hubertszostek@gmail.com>
2014-03-08 11:10:24 +0100

    Cluster data

---
+++ src/scripts/clusterData.py
@@ -0,0 +1,8 @@
+class clusterData:
+    
+    def __init__(self):
+        self.clusterToRanker = {}  # This should contain a hashmap. Key: number of cluster (integer). Value: ranker array
+        self.queryToCluster={} # This should contain a hasmpa. Key: query. Value: number of cluster 
+    
+
+
